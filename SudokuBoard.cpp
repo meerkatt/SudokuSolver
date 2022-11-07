203983ee6a702ce7861c819048eccd0a44d11ea5
meerkatt <meergujral@gmail.com>
2022-11-06 18:31:49 -0600

    Initial File Setup

---
+++ SudokuBoard.cpp
@@ -0,0 +1,7 @@
+#include "SudokuBoard.h"
+
+int* new_board(){
+    int* board = new int[81]; 
+    return board;
+}
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "SudokuSolver.cpp"
+#include <iostream>
+
+// This file includes the main function which will be used for running the Sudoku Solver Function.
+
+int main(){
+    int
+}
